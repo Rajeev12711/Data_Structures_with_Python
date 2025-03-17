406e4ad31af00056ddd7b7a7e124e7bc67c89c92
Rajeev <rajeev782772@gmail.com>
2025-03-17 20:11:34 +0530

    Python

---
+++ Stacks/LeetCode/Min_Stack.py
@@ -0,0 +1,19 @@
+class MinStack(object):
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val):
+        self.stack.append(val)
+        val= min(val, self.min_stack[-1] if self.min_stack else val)
+        self.min_stack.append(val)
+
+    def pop(self):
+        self.stack.pop()
+
+    def top(self):
+        return self.stack[-1]
+
+    def getmin(self):
+        return self.min_stack[-1]
+
