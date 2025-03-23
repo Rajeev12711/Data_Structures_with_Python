7f11dc3787b3593a1918c18f3768b6b79bb55b1d
Rajeev <rajeev782772@gmail.com>
2025-03-23 10:39:04 +0530

    Python

--- Dynamic Array/LeetCode/Concatenation of Array.py
+++ Dynamic Array/LeetCode/Concatenation of Array.py
@@ -1,7 +1,12 @@
 def concatenation_array(nums):
-    arr = nums[:]
-    arr = arr + nums
+    # arr = nums[:]
+    # arr = arr + nums
+    # return arr
+
+    arr = nums[:]+nums[:]
     return arr
+
+
 
 box =[1,3,2,1]
 print(concatenation_array(box))
---
+++ Extra Question/Fruits Into Baskets II.py
@@ -0,0 +1,8 @@
+fruits=[1,3,6]
+baskets=[2,5,2]
+for z in fruits:
+    for y in baskets:
+        if z <= y:
+            baskets.remove(y)
+            break
+print(len(baskets))
