68db88d45da2551e6f28e3d256afc948fc1d7711
Rajeev <rajeev782772@gmail.com>
2025-07-22 00:35:55 +0530

    Python

---
+++ Recrsion/Leetcode/Climbing Stairs.py
@@ -0,0 +1,13 @@
+def climbStairs(n):
+    if n in [0, 1, 2]:
+        return n
+    else:
+        prev1 = 1
+        prev2 = 1
+        for i in range(2, n + 1):
+            cur = prev1 + prev2
+            prev1 = prev2
+            prev2 = cur
+        return cur
+
+print(climbStairs(5))
